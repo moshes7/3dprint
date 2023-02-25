3d31057157f8bf274ac588c41004075eb4c1f474
moshes7 <moshes777@gmail.com>
2023-02-26 01:06:51 +0200

    add requirements.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='3dprint',
+      version='0.1',
+      description='Process images for 3D pringint',
+      author='Moshe Shilemay',
+      author_email='moshes777@gmail.com',
+      packages=find_packages(),
+      )
